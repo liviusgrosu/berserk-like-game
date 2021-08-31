4a387ab6195ef5d4f50094c5b5090fb342c912a8
liviusgrosu <livius.grosu@ryerson.ca>
2021-08-31 08:00:51 -0300

    Started the entity stats script

---
+++ Assets/Scripts/Entity/EntityStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityStats : MonoBehaviour
+{
+    // Stats
+    public int Health;
+    public int Stamina;
+    public int StaminaRegeneration;
+    public int Defence;
+    public int AttackSpeed;
+
+    // Current
+    public int CurrentHealth;
+    public int CurrentStamina;
+}
