edbfcb14f0917906830b62d8bd078b33f2bf0446
liviusgrosu <livius.grosu@ryerson.ca>
2021-10-11 15:51:47 -0400

    Changed colour scheme of nav bar buttons when switching between menus

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -17,6 +17,7 @@ public class MenuController : MonoBehaviour
     public Menu _currentMenu;
 
     public GameObject OverlayUI, WeaponUpgradeMenu, CharacterSkillMenu, NavBar;
+    public NavBarButton CharacterSkillsBtn, WeaponUpgradeBtn;
 
     void Awake()
     {
@@ -47,10 +48,31 @@ public class MenuController : MonoBehaviour
 
     private void DeactivateMenus()
     {
+        // Deactivate all the menus
         NavBar.SetActive(false);
         OverlayUI.SetActive(false);
         WeaponUpgradeMenu.SetActive(false);
         CharacterSkillMenu.SetActive(false);
+    }
+
+    private void UpdateNavBar()
+    {
+        // Refresh the selections
+        CharacterSkillsBtn.ChangeSelected(false);
+        WeaponUpgradeBtn.ChangeSelected(false);
+
+        // Toggle the current menu button
+        switch(_currentMenu)
+        {
+            case Menu.CharacterSkills:
+                CharacterSkillsBtn.ChangeSelected(true);
+                break;
+            case Menu.WeaponUpgrade:
+                WeaponUpgradeBtn.ChangeSelected(true);
+                break;
+            default:
+                break;
+        }
     }
 
     public void ChangeMenu(NavBarButton NavBarButton)
@@ -65,18 +87,23 @@ public class MenuController : MonoBehaviour
         switch (menu)
         {
             case Menu.CharacterSkills:
+                // Toggle all the character skills UI elements
                 _currentMenu = Menu.CharacterSkills;
                 CharacterSkillMenu.SetActive(true);
                 NavBar.SetActive(true);
+                UpdateNavBar();
                 break;
             case Menu.Overlay:
+                // Toggle all overlay UI elements
                 _currentMenu = Menu.Overlay;
                 OverlayUI.SetActive(true);
                 break;
             case Menu.WeaponUpgrade:
+                // Toggle all the weapon upgrade UI elements
                 _currentMenu = Menu.WeaponUpgrade;
                 WeaponUpgradeMenu.SetActive(true);
                 NavBar.SetActive(true);
+                UpdateNavBar();
                 break;
             default:
                 break;
--- Assets/Scripts/UI/NavBarButton.cs
+++ Assets/Scripts/UI/NavBarButton.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NavBarButton : MonoBehaviour
 {
     public MenuController.Menu MenuToSwitchTo;
+    private Button _button;
+    private ColorBlock _colours;
+
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+        _colours = _button.colors;
+    }
+
+    public void ChangeSelected(bool toggleState)
+    {
+        _button.interactable = !toggleState;
+        Color newColour = toggleState ? _colours.selectedColor : _colours.normalColor;
+        GetComponent<Image>().color = newColour;
+    }
 }
