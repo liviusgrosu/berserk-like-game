d6561db92c8d0b18cee7d61806c9a8d8ef6f64e1
liviusgrosu <livius.grosu@ryerson.ca>
2021-10-11 19:55:49 -0400

    Added basic rolling feature

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rigidbody;
-    public Transform DirectionCameraOffset;
+    // Movement variables
     public float MovementSpeed = 5f;
-    public float TurningSpeed = 10f;
-    
+    public float MovementTime = 0.8f;
     private RaycastHit mouseRay;
-
+    // Rotation variables
+    public Transform DirectionCameraOffset;
+    public float TurningSpeed = 10f;
+    // Rolling variables
+    public float RollingSpeed = 1.5f;
+    public float RollingTime = 1.0f;
+    private float _currentRollingTime;
+    private bool _isRolling;
+    private Vector3 _rollingDirection;
+    // Misc.
     public GeneratePath GridGenerator;
+    private CapsuleCollider _collider;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
     }
 
     void Start()
@@ -25,6 +35,9 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        HandleInput();
+
+        // --- Rotation ---
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -36,14 +49,45 @@ public class PlayerMovement : MonoBehaviour
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         }
 
-        Vector3 movementDirection = Vector3.zero;
+        // --- Movement ---
+        if (_isRolling)
+        {
+            rigidbody.velocity = _rollingDirection * RollingSpeed;
+            _currentRollingTime -= Time.deltaTime;
+            if (_currentRollingTime <= 0.0f)
+            {
+                _isRolling = false;
+                // Change collider
+                _collider.center += new Vector3(0.0f, 0.5f, 0.0f);
+                _collider.height *= 2f;
+            }
+        }
+        else
+        {
+            Vector3 movementDirection = Vector3.zero;
 
-        // Move character relative to the camera rotation offset
-        movementDirection += DirectionCameraOffset.forward * Input.GetAxisRaw("Vertical");
-        movementDirection += DirectionCameraOffset.right * Input.GetAxisRaw("Horizontal");
+            // Move character relative to the camera rotation offset
+            movementDirection += DirectionCameraOffset.forward * Input.GetAxisRaw("Vertical");
+            movementDirection += DirectionCameraOffset.right * Input.GetAxisRaw("Horizontal");
 
-        // Eliminate double speed with multiple inputs
-        movementDirection = movementDirection.normalized;
-        rigidbody.velocity = movementDirection * MovementSpeed;
+            // Eliminate double speed with multiple inputs
+            movementDirection = movementDirection.normalized;
+            rigidbody.velocity = movementDirection * MovementSpeed;
+        }
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !_isRolling)
+        {
+            // Start the rolling countdown
+            _currentRollingTime = RollingTime;
+            _isRolling = true;
+            // Get previous movement direction
+            _rollingDirection = rigidbody.velocity / MovementSpeed;
+            // Change collider
+            _collider.height /= 2f;
+            _collider.center -= new Vector3(0.0f, 0.5f, 0.0f);
+        }
     }
 }
