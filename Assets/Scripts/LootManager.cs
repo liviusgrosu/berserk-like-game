78d6c2331cb86e840526d10cb69f1798cfe32210
liviusgrosu <livius.grosu@ryerson.ca>
2021-10-25 19:31:57 -0400

    Implemented saving/loading of loot data

--- Assets/Scripts/LootManager.cs
+++ Assets/Scripts/LootManager.cs
@@ -1,9 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class LootManager : MonoBehaviour
 {
-    public int Experience = 500;
-    public int Gems = 100;
+    public int Experience;
+    public int Gems;
+
+    void Awake()
+    {
+        Load();
+    }
+
+    private void Load()
+    {
+        if (File.Exists($"{Application.persistentDataPath}/loot.inventory"))
+        {
+            // Get the file and data
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open($"{Application.persistentDataPath}/loot.inventory", FileMode.Open);
+            (int Experience, int Gems) lootData = ((int, int))bf.Deserialize(file);
+            
+            // Assign new data
+            Experience = lootData.Experience;
+            Gems = lootData.Gems;
+            
+            file.Close();
+        }
+        else 
+        {
+            // Default values
+            Experience = 500;
+            Gems = 100;
+        }
+    }
+    private void Save()
+    {
+        // Save loot
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create($"{Application.persistentDataPath}/loot.inventory");
+        bf.Serialize(file, (Experience, Gems));
+        file.Close();
+    }
+
+    void OnApplicationQuit()
+    {
+        Save();
+    }
 }
