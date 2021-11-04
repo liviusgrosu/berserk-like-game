3ed239769cabffb89db2c010948b9f60068b7c59
liviusgrosu <livius.grosu@ryerson.ca>
2021-11-04 18:52:20 -0400

    Defined max player health and stamina and sized the player stats UI correctly

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+class Constants : MonoBehaviour
+{
+    public float MaxPlayerLife = 100.0f;
+    public float MaxPlayerStamina = 100.0f;
+}
--- Assets/Scripts/Player/PlayerStatsUI.cs
+++ Assets/Scripts/Player/PlayerStatsUI.cs
@@ -10,22 +10,27 @@ class PlayerStatsUI : MonoBehaviour
     public Slider StaminaBarSlider;
 
     public EntityStats EntityStats;
+    public Constants Constants;
 
     private float _originalHealthBarSize, _originalStaminaBarSize;
+    private float _maxSliderSize;
 
     void Start()
     {
+        _maxSliderSize = GetComponent<RectTransform>().sizeDelta.x;
+
+        // Setup the base stats size
         _originalHealthBarSize = HealthBar.sizeDelta.x;
         _originalStaminaBarSize = StaminaBar.sizeDelta.x;
 
         // Health Bar
         HealthBarSlider.maxValue = EntityStats.Stats.Health;
-        HealthBar.sizeDelta = new Vector2(_originalHealthBarSize * EntityStats.Stats.Health, HealthBar.sizeDelta.y);
+        HealthBar.sizeDelta = new Vector2(_maxSliderSize * (EntityStats.Stats.Health / Constants.MaxPlayerLife), HealthBar.sizeDelta.y);
         HealthBarSlider.value = EntityStats.CurrentHealth;
 
         // Stamina Bar
         StaminaBarSlider.maxValue = EntityStats.Stats.Stamina;
-        StaminaBar.sizeDelta = new Vector2(_originalStaminaBarSize * EntityStats.Stats.Stamina, StaminaBar.sizeDelta.y);
+        StaminaBar.sizeDelta = new Vector2(_maxSliderSize * (EntityStats.Stats.Stamina / Constants.MaxPlayerStamina), StaminaBar.sizeDelta.y);
         StaminaBarSlider.value = EntityStats.CurrentStamina;
     }
 
@@ -35,14 +40,14 @@ class PlayerStatsUI : MonoBehaviour
         if (HealthBarSlider.maxValue != EntityStats.Stats.Health)
         {
             HealthBarSlider.maxValue = EntityStats.Stats.Health;
-            HealthBar.sizeDelta = new Vector2(_originalHealthBarSize * EntityStats.Stats.Health, HealthBar.sizeDelta.y);
+            HealthBar.sizeDelta = new Vector2(_maxSliderSize * (EntityStats.Stats.Health / Constants.MaxPlayerLife), HealthBar.sizeDelta.y);
         }
 
         // Stamina bar update
         if (StaminaBarSlider.maxValue != EntityStats.Stats.Stamina)
         {
             StaminaBarSlider.maxValue = EntityStats.Stats.Stamina;
-            StaminaBar.sizeDelta = new Vector2(_originalStaminaBarSize * EntityStats.Stats.Stamina, StaminaBar.sizeDelta.y);
+            StaminaBar.sizeDelta = new Vector2(_maxSliderSize * (EntityStats.Stats.Stamina / Constants.MaxPlayerStamina), StaminaBar.sizeDelta.y);
         }
 
         HealthBarSlider.value = EntityStats.CurrentHealth;
